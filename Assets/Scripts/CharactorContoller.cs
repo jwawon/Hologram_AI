d7fc4745b6a4a6ad7e72ebb6382a51872d199fd4
Jwawon <Jwawon@Superman.local>
2018-04-07 17:46:04 +0900

    20180407 First Commit for a Hologram Project. New controller for a UnityChan Character.

---
+++ Assets/Scripts/CharactorContoller.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CharactorContoller : MonoBehaviour {
+	public Animator ChanAnim;
+	public Text InputText;
+	float InitTime;
+	float ElapsedTime = 10f;
+	void Start() {
+		InitTime = Time.time;
+	}
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Return)){
+			if(InputText.text == "Hello" || InputText.text == "Hi") ChanAnim.SetTrigger("Hi");	
+			if(InputText.text == "You're wrong") ChanAnim.SetTrigger("Wrong");	
+			if(InputText.text == "You're right" || InputText.text == "Thank you") ChanAnim.SetTrigger("Yay");
+			if(Time.time - InitTime > ElapsedTime){
+				ChanAnim.SetBool("IsBored", true);
+				InitTime = Time.time;
+			}
+			else 
+				ChanAnim.SetBool("IsBored", false);
+		}
+	}
+}
