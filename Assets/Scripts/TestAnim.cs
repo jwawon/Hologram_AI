dff7b08f534042f5909760ba488bd14aa3789d0f
Jwawon <Jwawon@Superman.local>
2018-04-09 01:10:01 +0900

    Add Watson AI sdk

---
+++ Assets/Scripts/TestAnim.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TestAnim : MonoBehaviour {
+
+	public Animator testAnim;
+	public Text HelloText;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.Return)){
+			if(HelloText.text == "Hello"){
+				testAnim.SetTrigger("Hello");
+			}
+		}
+	}
+}
