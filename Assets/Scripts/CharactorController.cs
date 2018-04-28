cdd022f776997379c7c0cb9826d2de1a2bd2cd41
Jwawon <Jwawon@Superman.local>
2018-04-28 20:10:02 +0900

    add Restful API

--- Assets/Scripts/CharactorController.cs
+++ Assets/Scripts/CharactorController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class CharactorContoller : MonoBehaviour {
+public class CharactorController : MonoBehaviour {
 	public Animator ChanAnim;
 	public Text InputText;
 	float InitTime;
