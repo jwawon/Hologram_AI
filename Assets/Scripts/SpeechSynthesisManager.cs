2f439b667752dadf77839a43f0feca84af5455c1
Jwawon <>
2018-05-22 18:57:22 +0900

    play audio of speech synthesis

---
+++ Assets/Scripts/SpeechSynthesisManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeechSynthesisManager : MonoBehaviour {
+
+	AudioSource source;
+	string url = "http://61.85.36.59:9000/synthesize?text=hello%20world";
+	void Start () {
+		StartCoroutine(PlayText());
+	}
+	
+    IEnumerator PlayText()
+    {
+        source = GetComponent<AudioSource>();
+		var www = new WWW(url);
+		yield return www;
+		Debug.Log(www);
+		Debug.Log("test");
+		source.clip = www.GetAudioClip();
+        if (!source.isPlaying)
+        {
+            source.Play();
+        }
+    }
+}
